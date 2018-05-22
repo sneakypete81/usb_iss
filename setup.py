01a20abf0f8958ea727e36f95c5e4c121db82a75
sneakypete81 <sneakypete81@gmail.com>
2018-05-22 19:57:07 +0100

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/sneakypete81/usb_iss',
-    version='0.2.1',
+    version='0.2.2',
     zip_safe=False,
 )
