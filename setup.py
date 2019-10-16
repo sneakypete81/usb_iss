4a5f56db61a0f8a2f575d1ba20c7cfe46b9c9981
sneakypete81 <sneakypete81@users.noreply.github.com>
2019-10-16 22:35:31 +0100

    Now a stable release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author="Pete Burgers",
     author_email='sneakypete81@gmail.com',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
